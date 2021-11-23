6417d6824d777b1fcad4153b7ab7f022981d01b8
dby-lgth <74655793+dby-lgth@users.noreply.github.com>
2021-11-23 19:00:54 +0800

    Update GitTest.java

--- src/main/java/cn/baisee/Git/GitTest.java
+++ src/main/java/cn/baisee/Git/GitTest.java
@@ -12,6 +12,7 @@ public class GitTest {
         System.out.println("3333");
         System.out.println("hot-fix");
         System.out.println("push test");
+        System.out.println("pull test");
 
     }
 
